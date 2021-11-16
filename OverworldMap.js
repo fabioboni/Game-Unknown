47ad1fff4bde4c0c85ebe7dbf6cf3e8525b79de7
fabioboni <boni@fabioboni.com>
2021-11-16 12:35:44 -0300

    primeiro commit, base e alguns assets prontos

---
+++ OverworldMap.js
@@ -0,0 +1,13 @@
+class OverworldMap {
+    constructor(config) {
+        this.gameObjects = config.gameObjects;
+
+        this.lowerImage = new Image();
+        this.lowerImage.src = config.lowerSrc;
+
+        this.upperImage = new Image();
+        this.upperImage.src = config.upperSrc;
+    }
+}
+
+
